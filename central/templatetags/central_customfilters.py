a7c8aabdfc0779f02c764aa2082c5bf559701c3e
Harmen Hoek <harmen@myhoek.nl>
2022-04-30 21:40:04 +0200

    planner base

--- central/templatetags/central_customfilters.py
+++ central/templatetags/central_customfilters.py
@@ -52,7 +52,7 @@ def noneto_description(value):
 
 @register.filter
 def FancyTrueFalse(value):
-    if True:
+    if value:
         return "<i class='fas fa-check'></i>"
     else:
         return "<i class='fas fa-times'></i>"
